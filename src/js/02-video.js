59a423963c1685973b3a1cb745d81d7d939167b7
DmytroPrymachenko <144675672+DmytroPrymachenko@users.noreply.github.com>
2023-11-08 17:11:30 +0200

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,42 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('.js-iframe-video');
+const player = new Player(iframe, {
+  id: 19231868,
+  width: 640,
+});
+
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+player
+  .setCurrentTime(loadToLS('videoplayer-current-time'))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
+
+function onTimeupdate(data) {
+  saveToLS('videoplayer-current-time', data.seconds);
+}
+function saveToLS(key, value) {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+function loadToLS(key) {
+  try {
+    return JSON.parse(localStorage.getItem(key)) || {};
+  } catch (error) {
+    console.log(error.message);
+    return localStorage.getItem(key);
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
+const refs = {
+  formElem: document.querySelector('.feedback-form'),
+};
 
+refs.formElem.addEventListener('input', throttle(onFormElemInput, 500));
+refs.formElem.addEventListener('submit', onFormElemSubmit);
+
+function onFormElemInput(elem) {
+  elem.preventDefault();
+
+  const userForm = {};
+  userForm.userEmail = refs.formElem.elements.email.value;
+  userForm.userMessage = refs.formElem.elements.message.value;
+  saveTols('userKey', userForm);
+}
+
+function saveTols(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+function loadFromLS(key) {
+  const data = localStorage.getItem(key);
+  try {
+    return JSON.parse(data);
+  } catch (err) {
+    return data;
+  }
+}
+
+function onload() {
+  const data = loadFromLS('userKey');
+}
+function onFormElemSubmit(elem) {
+  elem.preventDefault();
+  localStorage.removeItem('userKey');
+  const data = loadFromLS('userKey');
+
+  elem.target.reset();
+}
+
+onload();
